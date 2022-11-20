cd0741991b75266802e8ef2e08dc43ac595972ef
Aman123lug <ak06465676@gmail.com>
2022-11-20 23:21:28 +0530

    initial commit

---
+++ train.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+import re
+import string
+from wordcloud import WordCloud
+from collections import Counter
+
+import warnings
+warnings.filterwarnings('ignore')
+
+from nltk import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+# from scikitplot.metrics import plot_confusion_matrix, plot_roc
+
+
+
+
+data = pd.read_csv('spam.csv', encoding="latin-1")
+
+data['Total Words'] = data['v2'].apply(lambda x: len(x.split()))
+
+def count_total_words(text):
+    char = 0
+    for word in text.split():
+        char += len(word)
+    return char
+
+data['Total Chars'] = data["v2"].apply(count_total_words)
+data.drop(columns=["Unnamed: 2","Unnamed: 3", "Unnamed: 4"], inplace=True)
+data.rename(columns={"v1":"Category", "v2":"Message"}, inplace=True)
+def convert_lowercase(text):
+    text = text.lower()
+    return text
+
+data['Message'] = data['Message'].apply(convert_lowercase)
+def remove_url(text):
+    re_url = re.compile('https?://\S+|www\.\S+')
+    return re_url.sub('', text)
+
+data['Message'] = data['Message'].apply(remove_url)
+
+exclude = string.punctuation
+
+def remove_punc(text):
+    return text.translate(str.maketrans('', '', exclude))
+
+data['Message'] = data['Message'].apply(remove_punc)
+
+
+data['Total Words After Transformation'] = data['Message'].apply(lambda x: np.log(len(x.split())))
+
+data['Category'] = data['Category'].replace({'spam':0,'ham':1})
+
+X = data["Message"]
+y = data['Category'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state= 42, stratify = y)
+tfidf = TfidfVectorizer(max_features= 2500, min_df= 2)
+X_train = tfidf.fit_transform(X_train).toarray()
+X_test = tfidf.transform(X_test).toarray()
+
+def train_model(model):
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    y_prob = model.predict_proba(X_test)
+    accuracy = round(accuracy_score(y_test, y_pred), 3)
+    precision = round(precision_score(y_test, y_pred), 3)
+    recall = round(recall_score(y_test, y_pred), 3)
+
+    print(f'Accuracy of the model: {accuracy}')
+    print(f'Precision Score of the model: {precision}')
+    print(f'Recall Score of the model: {recall}')
+    
+    
+rf = RandomForestClassifier(n_estimators= 300)
+a= train_model(rf)
+print(a)
+
+
